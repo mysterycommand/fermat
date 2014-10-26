fded1775a8c53477b27d9ad8280affec1f8d8853
Matt Hayes <matt@mysterycommand.com>
2014-10-25 22:53:11 -0700

    JSHint is the first task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
 var gulp = require('gulp');
 
-gulp.task('default');
+(function(dirs) {
+    if (! util.isArray(dirs)) { dirs = [dirs]; }
+
+    dirs.forEach(function(dir) {
+        fs.readdirSync(dir).forEach(function(file) {
+            var f = path.resolve(dir, file);
+            if (! fs.statSync(f).isFile()) { return; }
+            require(f);
+        });
+    });
+}([
+    './gulp/plugin',
+    './gulp/custom'
+]));
+
+gulp.task('default', ['jshint']);
